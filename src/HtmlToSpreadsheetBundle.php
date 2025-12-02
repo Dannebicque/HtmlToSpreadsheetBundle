71dacb16b8cf7d357a56035d3b470f846e13a744
davidannebicque <david.annebicque@gmail.com>
2025-12-02 10:52:11 +0100

    fix configuration

--- src/HtmlToSpreadsheetBundle.php
+++ src/HtmlToSpreadsheetBundle.php
@@ -4,34 +4,66 @@ namespace Davidannebicque\HtmlToSpreadsheetBundle;
 
 use Davidannebicque\HtmlToSpreadsheetBundle\DependencyInjection\ConfigureServicesPass;
 use Davidannebicque\HtmlToSpreadsheetBundle\Spreadsheet\Styler\StylePresets;
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class HtmlToSpreadsheetBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition
+            ->rootNode()
+            ->children()
+            ->scalarNode('temp_dir')
+            ->defaultNull()
+            ->info('Répertoire temporaire pour les fichiers')
+            ->end()
+            ->booleanNode('strict')
+            ->defaultTrue()
+            ->info('Activer le mode strict pour la validation HTML')
+            ->end()
+            ->booleanNode('include_builtins')
+            ->defaultTrue()
+            ->info('Inclure les styles prédéfinis du bundle')
+            ->end()
+            ->arrayNode('default_styles')
+            ->info('Définir vos styles personnalisés')
+            ->example([
+                'custom_header' => [
+                    'font' => ['bold' => true, 'color' => 'FFFFFF'],
+                    'fill' => ['fillType' => 'solid', 'color' => '4472C4'],
+                ]
+            ])
+            ->useAttributeAsKey('name')
+            ->arrayPrototype()
+            ->variablePrototype()->end()
+            ->end()
+            ->end()
+            ->end();
+    }
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('./Resources/config/services.yaml');
 
         $builtins = [
-            'th'               => StylePresets::header(),
-            'int'              => StylePresets::int(),
-            'float2'           => StylePresets::float2(),
-            'float3'           => StylePresets::float3(),
-            'percent2'         => StylePresets::percent2(),
-            'money'            => StylePresets::moneyEuro(),
+            'th' => StylePresets::header(),
+            'int' => StylePresets::int(),
+            'float2' => StylePresets::float2(),
+            'float3' => StylePresets::float3(),
+            'percent2' => StylePresets::percent2(),
+            'money' => StylePresets::moneyEuro(),
             'money_accounting' => StylePresets::moneyEuroAccounting(),
-            'date'             => StylePresets::dateShort(),
-            'date_long'        => StylePresets::dateLongFr(),
-            'time'             => StylePresets::timeShort(),
-            'datetime'         => StylePresets::dateTime(),
-            'duration'         => StylePresets::duration(),
-            'bool_center'      => StylePresets::boolCentered(),
-            'text_wrap'        => StylePresets::textWrap(),
-            'warn'             => StylePresets::warning(),
+            'date' => StylePresets::dateShort(),
+            'date_long' => StylePresets::dateLongFr(),
+            'time' => StylePresets::timeShort(),
+            'datetime' => StylePresets::dateTime(),
+            'duration' => StylePresets::duration(),
+            'bool_center' => StylePresets::boolCentered(),
+            'text_wrap' => StylePresets::textWrap(),
+            'warn' => StylePresets::warning(),
         ];
 
         $includeBuiltins = $config['include_builtins'] ?? true;
